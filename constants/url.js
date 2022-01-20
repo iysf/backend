6fc779f07027fc889632c0b7ce3f75e59be123ed
杨少峰 <yangshaofengi@didiglobal.com>
2022-01-20 21:54:13 +0800

    .

--- constants/url.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  dataBase: 'mongodb://houxiaochen:zhaozhao719@docdb-2022-01-20-10-20-34.cb4jxav3ugif.ap-south-1.docdb.amazonaws.com:27017/?retryWrites=false'
-}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const url = require('../constants/url')
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-mongoose.connect(url.dataBase, { useNewUrlParser: true }, (err) => {
-  console.log('err:', err)
-  console.log('连接成功:', err)
-})
+mongoose.connect(
+  'mongodb://houxiaochen:zhaozhao719@docdb-2022-01-20-10-20-34.cb4jxav3ugif.ap-south-1.docdb.amazonaws.com:27017/rankingList?retryWrites=false&authSource=admin',
+  { useNewUrlParser: true },
+  (err) => {
+    console.log('err:', err)
+    console.log('连接成功:', err)
+    const SaveSchema = Schema({ result: Number })
+
+    const SaveModal = mongoose.modal('cats', SaveSchema)
+    const instance = new CatModal({ result: 20 })
+
+    instance.save((e, res) => {
+      console.log('e:', e)
+      console.log('res:', res)
+    })
+  }
+)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
