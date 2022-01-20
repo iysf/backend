abec5c264f95bf50992bfecb3f10be2753f4b8ea
杨少峰 <yangshaofengi@didiglobal.com>
2022-01-20 21:08:58 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+const url = require('../constants/url')
+const mongoose = require('mongoose')
+
+mongoose.connect(url.dataBase)
+
+const db = mongoose.connection
+
+db.on('connected', (err) => {
+  console.log('err:', err)
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api/search/ranking', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.send({
+    code: 0,
+    data: {
+      rankingList: [
+        { result: 958 },
+        { result: 800 },
+        { result: 754 },
+        { result: 425 },
+        { result: 251 },
+      ]
+    },
+    success: true
+  })
+})
+
+router.post('/api/save', (req, res, next) => {
+  console.log('req:', req)
+
+  res.send({
+    code: 0,
+    data: {},
+    success: true
+  })
+})
+
+module.exports = router;
