c4b473630a4f5b117c79310179055393f05d29ef
杨少峰 <yangshaofengi@didiglobal.com>
2022-01-22 16:40:04 +0800

    完善node逻辑

--- routes/index.js
+++ routes/index.js
@@ -29,30 +29,77 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/search/ranking', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-  mongoose.model('result', SaveSchema).find({}, 'result -_id', (err, docs) => {
+  mongoose.model('result', SaveSchema).find({}, 'result -_id').sort({ result: -1 }).limit(5).exec((error, result) => {
+    if (error) {
+      res.send({
+        code: 1,
+        data: '查询失败',
+        success: false
+      })
+    } else {
+      res.send({
+        code: 0,
+        data: {
+          rankingList: result
+        },
+        success: true
+      })
+    }
+  })
+})
+
+router.get('/api/save', async (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  const query = req.query
+
+  if (!query.result) {
     res.send({
-      code: 0,
-      data: {
-        rankingList: docs
-      },
-      success: true
+      code: 1,
+      data: '请传入成绩',
+      success: false
+    })
+    return
+  }
+
+  const { result } = query
+  const ResultModal = mongoose.model('result', SaveSchema)
+
+  // 去重，假如已存在该成绩，则不保存。
+  const docs = await new Promise((resolve, reject) => {
+    ResultModal.find({ result: result }, 'result -_id', (err, docs) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(docs)
+      }
     })
   })
 
-})
-
-router.post('/api/save', (req, res, next) => {
-  console.log('req:', req)
-
-  // const SaveModal = mongoose.model('result', SaveSchema).insertMany({ result: 852 }, (error, res) => {
-  //   if (error) {
-  //     console.log(error)
-  //   } else {
-  //     console.log(`SaveSchema 表，'insert ok'`)
-  //   }
-  // })
+  if (docs && docs.length) {
+    console.log('数据重复了')
+    res.send({
+      code: 0,
+      data: {},
+      success: true
+    })
+    return
+  }
+  const insertSuccess = await new Promise((resolve, reject) => {
+    ResultModal.insertMany({ result: result }, (error, success) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve({})
+      }
+    })
+  })
 
   res.send({
     code: 0,
